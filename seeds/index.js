3e80f67479160addbe62d0ead85e5f035575908f
bgriffy <brentgriffin94@gmail.com>
2021-03-15 14:04:42 -0400

    Add fixed sizing for images on workspace index

--- seeds/index.js
+++ seeds/index.js
@@ -3,8 +3,14 @@ const Workspace = (require("../models/workspace"));
 const User = (require("../models/user"));
 const cities = require("./cities");
 const { places, descriptors } = require("./seedhelpers");
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/crammify";
 
-mongoose.connect('mongodb://localhost:27017/crammify', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
 
 const db = mongoose.connection;
 
