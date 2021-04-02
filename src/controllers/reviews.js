879e074b564ffbb5b99899ee68d6a9022f893d6c
bgriffy <brentgriffin94@gmail.com>
2021-04-02 13:02:12 -0400

    Added logic to get average lighting level for reviews
    - Added logic to log average lighting level to console
    - TODO: Add logic to save average lighting level to workspace
    - TODO: Add same functionality for getting avg for all other review attributes

--- src/controllers/reviews.js
+++ src/controllers/reviews.js
@@ -14,6 +14,19 @@ module.exports.createReview = async (req, res, next) => {
     await newReview.save();
     await workspace.save();
 
+    // Get the array of review IDs associated with the workspace
+    Workspace.findById(id, "reviews", (err, workspace) => {
+        // Get the actual review objects associated with those IDs and get average lighting level 
+        Review.aggregate(
+            [
+                { $match: { _id: { $in: workspace.reviews } } },
+                { $group: { _id: null, average: { $avg: "$lightingLevel" } } }
+            ], (err, result) => {
+                console.log(`AGGREGATE RESULT: ${result.average}`);}
+            );
+    });
+
+
     req.flash("success", "Your review has been added successfully.");
     res.redirect(`/workspaces/${id}`);
 };
